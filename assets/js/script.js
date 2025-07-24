ff032f7e71ec7e366f24e42d40bbda8adfb8d7d0
Maca <maca.motions@gmail.com>
2025-07-23 20:20:24 -0400

    codigo añadido

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+header {
+    font-size: 5rem;
+    text-align: center;
+    background-color: rgb(95, 223, 95);
+    padding-top: 2%;
+    padding-bottom: 2%;
+    font-family: "Roboto", sans-serif;
+    color: white;
+}
+
+.container {    
+    margin: 0 auto;
+    padding: 2%;
+    font-family: "Roboto", sans-serif;
+}
+
+.container ul {
+    font-size: 1.5rem;
+}
